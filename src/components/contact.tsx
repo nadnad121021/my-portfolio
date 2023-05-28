7f12221a14c6ec1d9059dd764d535a5cbf8aeb1a
nadAlingasa <dan.alingasa@edify.ai>
2023-05-28 11:58:50 +0800

    refactor: update contacts

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -62,37 +62,40 @@ const Contact: React.FC = () => {
 
           <div className="mx-auto col-lg-4 col-md-6 col-12">
             <h3 className="my-4 pt-4 pt-lg-0">Keep in Touch</h3>
-            <p className="mb-1">+639365192469</p>
-            <p>
+            <p className="mb-1">
               {/* eslint-disable-next-line */}
-              <a href="#">
-                alingasadan@gmail.com
-                <i className="fas fa-arrow-right custom-icon"></i>
-              </a>
+              <a href="javascript:void(0)" className="fas fa-phone"></a>
+              {" +639365192469"}
             </p>
-            <ul className="social-links mt-2">
-              <li>
-                <a
-                  href="https://www.facebook.com/danilo.alingasa.7"
-                  rel="noopener"
-                  className="fab fa-facebook"
-                >
-                  {""}
-                </a>
-              </li>
-              {/* <li><a href="#" rel="noopener" className="fab fa-twitter"></a></li> */}
-              {/* <li><a href="#" rel="noopener" className="fab fa-instagram"></a></li> */}
-              <li>
-                <a
-                  href="https://www.linkedin.com/feed/"
-                  rel="noopener"
-                  className="fab fa-linkedin"
-                >
-                  {" "}
-                </a>
-              </li>
-              {/* <li><a href="#" rel="noopener" className="fab fa-youtube"></a></li> */}
-            </ul>
+            <p className="mb-1">
+              {/* eslint-disable-next-line */}
+              <a href="javascript:void(0)" className="fas fa-envelope"></a>
+              {"  alingasadan@gmail.com"}
+            </p>
+
+            <p className="mb-1">
+              {/* eslint-disable-next-line */}
+              <a
+                href="https://www.facebook.com/danilo.alingasa.7"
+                target="_blank"
+                className="fab fa-facebook"
+              ></a>
+              {" Danilo Alingasa"}
+            </p>
+            <p className="mb-1">
+              {/* eslint-disable-next-line */}
+              <a href="javascript:void(0)" className="fab fa-skype"></a>
+              {"  live:.cid.a08eee29df0e2c17"}
+            </p>
+            <p className="mb-1">
+              {/* eslint-disable-next-line */}
+              <a
+                href="https://www.linkedin.com/in/danilo-alingasa-a727241a2/"
+                target="_blank"
+                className="fab fa-linkedin"
+              ></a>
+              {"  Danilo Alingasa"}
+            </p>
 
             <p className="copyright-text mt-5 pt-3">
               Copyright &copy; 2023 Dan Page
